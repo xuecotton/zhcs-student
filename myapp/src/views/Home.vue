b30c8848885567018a1a973eca4ace4dfab9855f
kaikaijie <kjhuang0129@163.com>
2021-01-07 18:30:22 +0800

    cotton初始化

--- myapp/src/views/Home.vue
+++ myapp/src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <van-button type="primary">hello cotton</van-button>
+    <van-button type="danger">hello cotton</van-button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
