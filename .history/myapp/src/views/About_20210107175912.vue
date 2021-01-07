0756f2d3cb145141704840acf042dab3c51e5080
Volcano-del <1317765362@qq.com>
2021-01-07 18:00:02 +0800

    cc分支创建

--- .history/myapp/src/views/About_20210107175912.vue
+++ .history/myapp/src/views/About_20210107175912.vue
@@ -3,6 +3,7 @@
     <h1>This is an about page</h1>
     <van-button>Button</van-button>
     <van-button type="primary">按钮</van-button>
+    
     <van-steps :active="2">
       <van-step>买家下单</van-step>
       <van-step>商家接单</van-step>
